18dd89fb2fef555fc97a58737ad9579e5a0da99a
Ben Tulchinski (Blue Sun Games) <Ben.tulchinski@gmail.com>
2026-06-16 14:33:10 +0200

    Added Base Box Widget Utils Lib

---
+++ Plugins/BaseBox/Source/BaseBox/Private/Widgets/BxWidgetUtils.cpp
@@ -0,0 +1,64 @@
+﻿// Copyright Chaos Box Games 2026, All rights reserved.
+
+
+#include "Widgets/BxWidgetUtils.h"
+
+#include "Blueprint/SlateBlueprintLibrary.h"
+#include "Blueprint/UserWidget.h"
+
+int32 UBxWidgetUtils::CoordinateToArrayIndex(const FIntPoint& Coordinate, const int32 NumColumns)
+{
+	return Coordinate.Y * NumColumns + Coordinate.X;
+}
+
+FIntPoint UBxWidgetUtils::ArrayIndexToCoordinate(const int32 Index, const int32 NumColumns)
+{
+	return {Index % NumColumns, Index / NumColumns};
+}
+
+FVector2D UBxWidgetUtils::GetWidgetPosition(UWidget* Widget)
+{
+	const FVector2D AbsolutePosition = Widget->GetCachedGeometry().GetAbsolutePosition();
+	FVector2D PixelPosition;
+	FVector2D ViewportPosition;
+	USlateBlueprintLibrary::AbsoluteToViewport(Widget->GetOwningPlayer(), AbsolutePosition, PixelPosition, ViewportPosition);
+	return ViewportPosition;
+}
+
+FVector2D UBxWidgetUtils::GetWidgetCenter(UWidget* Widget)
+{
+	const FVector2D AbsolutePosition = Widget->GetCachedGeometry().GetAbsolutePosition();
+	const FVector2D AbsoluteSize = Widget->GetCachedGeometry().GetAbsoluteSize();
+	const FVector2D AbsoluteCenter = AbsolutePosition + AbsoluteSize / 2.f;
+	FVector2D PixelPosition;
+	FVector2D ViewportPosition;
+	USlateBlueprintLibrary::AbsoluteToViewport(Widget->GetOwningPlayer(), AbsoluteCenter, PixelPosition, ViewportPosition);
+	return ViewportPosition;
+}
+
+FVector2D UBxWidgetUtils::GetWidgetBottomRight(UWidget* Widget)
+{
+	const FVector2D AbsolutePosition = Widget->GetCachedGeometry().GetAbsolutePosition();
+	const FVector2D AbsoluteSize = Widget->GetCachedGeometry().GetAbsoluteSize();
+	const FVector2D AbsoluteBottomRight = AbsolutePosition + AbsoluteSize;
+	FVector2D PixelPosition;
+	FVector2D ViewportPosition;
+	USlateBlueprintLibrary::AbsoluteToViewport(Widget->GetOwningPlayer(), AbsoluteBottomRight, PixelPosition, ViewportPosition);
+	return ViewportPosition;
+}
+
+bool UBxWidgetUtils::IsPositionBoundByWidget(const FVector2D& ViewportPosition, UWidget* Widget)
+{
+	const FVector2D WidgetTopLeft = GetWidgetPosition(Widget);
+	const FVector2D WidgetBottomRight = GetWidgetBottomRight(Widget);
+	return ViewportPosition.ComponentwiseAllGreaterOrEqual(WidgetTopLeft) && ViewportPosition.ComponentwiseAllLessOrEqual(WidgetBottomRight);
+}
+
+bool UBxWidgetUtils::IsWidgetBoundByWidget(UWidget* SmallerWidget, UWidget* BiggerWidget)
+{
+	const FVector2D SmallerWidgetTopLeft = GetWidgetPosition(SmallerWidget);
+	const FVector2D SmallerWidgetBottomRight = GetWidgetBottomRight(SmallerWidget);
+	const FVector2D BiggerWidgetTopLeft = GetWidgetPosition(BiggerWidget);
+	const FVector2D BiggerWidgetBottomRight = GetWidgetBottomRight(BiggerWidget);
+	return SmallerWidgetTopLeft.ComponentwiseAllGreaterOrEqual(BiggerWidgetTopLeft) && SmallerWidgetBottomRight.ComponentwiseAllLessOrEqual(BiggerWidgetBottomRight);
+}
---
+++ Plugins/BaseBox/Source/BaseBox/Public/Widgets/BxWidgetUtils.h
@@ -0,0 +1,42 @@
+﻿// Copyright Chaos Box Games 2026, All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BxWidgetUtils.generated.h"
+
+class UWidget;
+/** Base Box Widget Utility Functions */
+UCLASS()
+class BASEBOX_API UBxWidgetUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintPure, Category="Inventory Widget Utils")
+	static int32 CoordinateToArrayIndex(const FIntPoint& Coordinate, const int32 NumColumns);
+
+	UFUNCTION(BlueprintPure, Category="Inventory Widget Utils")
+	static FIntPoint ArrayIndexToCoordinate(const int32 Index, const int32 NumColumns);
+	
+	/** Get Widget Top Left Corner in Viewport Coordinates */
+	UFUNCTION(BlueprintPure, Category="Inventory Widget Utils")
+	static FVector2D GetWidgetPosition(UWidget* Widget);
+	
+	/** Get Widget Center Position in Viewport Coordinates */
+	UFUNCTION(BlueprintPure, Category="Inventory Widget Utils")
+	static FVector2D GetWidgetCenter(UWidget* Widget);
+
+	/** Get Widget Bottom Right Corner Position in Viewport Coordinates */
+	UFUNCTION(BlueprintPure, Category="Inventory Widget Utils")
+	static FVector2D GetWidgetBottomRight(UWidget* Widget);
+	
+	/** Returns true if viewport position is within widget geometry */
+	UFUNCTION(BlueprintPure, Category="Inventory Widget Utils")
+	static bool IsPositionBoundByWidget(const FVector2D& ViewportPosition, UWidget* Widget);
+
+	/** Returns true if Smaller Widget is within Bigger Widget geometry */
+	UFUNCTION(BlueprintPure, Category="Inventory Widget Utils")
+	static bool IsWidgetBoundByWidget(UWidget* SmallerWidget, UWidget* BiggerWidget);
+};
